69942058054561e5103d4952e4be1f249a9c0c73
MelussiMkhonta <82477077+MelussiMkhonta@users.noreply.github.com>
2022-04-27 22:35:03 +0200

    Intro and Portfolio features

--- app/src/components/intro/Intro.jsx
+++ app/src/components/intro/Intro.jsx
@@ -1,6 +1,19 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Programmer"],
+    });
+  }, []);
+
   return (
     <div className='intro' id="intro">
       <div className="left">
@@ -9,15 +22,18 @@ export default function Intro() {
         </div>
       </div>
       
-      <div className="right"></div>
-        <div className="wrapper">
-          <h2>Hi There, I'm</h2>
-          <h1>Melusi Mkhonta</h1>
-          <h3>Freelancer <span></span></h3>
+        <div className="right">
+          <div className="wrapper">
+              <h2>Hi There, I'm</h2>
+              <h1>Melusi Mkhonta</h1>
+              <h3>
+                Freelance <span ref={textRef}></span>
+              </h3>
+          </div>
+          <a href="#portfolio">
+              <img src="assets/down.png" alt="" />
+          </a>
         </div>
-        <a href="#portfolio">
-          <img src="assets/down.png" alt="" />
-        </a>
     </div>
   )
 }
--- app/src/components/portfolio/Portfolio.jsx
+++ app/src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,52 @@ import "./portfolio.scss"
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-        
+        <h1>Portfolio</h1>
+        <ul>
+          <li className="active">Featured</li>
+          <li>Web Application</li>
+          <li>Mobile Application</li>
+          <li>Design</li>
+          <li>Branding</li>
+        </ul>
+        <div className="container">
+          <div className="item">
+            <img src="https://images.unsplash.com/photo-1565788061370-4771680a4df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=835&q=80"
+             alt="hey" 
+             />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://images.unsplash.com/photo-1565788061370-4771680a4df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=835&q=80"
+             alt="hey" 
+             />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://images.unsplash.com/photo-1565788061370-4771680a4df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=835&q=80"
+             alt="hey" 
+             />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://images.unsplash.com/photo-1565788061370-4771680a4df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=835&q=80"
+             alt="hey" 
+             />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://images.unsplash.com/photo-1565788061370-4771680a4df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=835&q=80"
+             alt="hey" 
+             />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://images.unsplash.com/photo-1565788061370-4771680a4df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=835&q=80"
+             alt="hey" 
+             />
+            <h3>Banking App</h3>
+          </div>
+        </div>
     </div>
   )
 }
