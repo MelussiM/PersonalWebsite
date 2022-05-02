83b8f7235e845200161e0ab5aea39de41f2aab7a
MelussiMkhonta <82477077+MelussiMkhonta@users.noreply.github.com>
2022-05-02 15:54:03 +0200

    Added dummy data, edited portfolio and works

--- app/src/components/portfolio/Portfolio.jsx
+++ app/src/components/portfolio/Portfolio.jsx
@@ -1,53 +1,80 @@
 import "./portfolio.scss"
+import PortfolioList from "../portfolioList/PortfolioList"
+import { useState } from "react";
+import { featuredPortfolio, webPortfolio, mobilePortfolio, designPortfolio, contentPortfolio} from "../../data";
+import { useEffect } from "react";
 
 export default function Portfolio() {
+const [selected, setSelected] = useState("featured");
+const [data, setData] = useState([]);
+
+
+  const list = [
+    {
+        id: "featured",
+        title:"Featured",
+    },
+    {
+      id: "web",
+      title:"Web App",
+    },
+    {
+      id: "mobile",
+      title:"Mobile App",
+    },
+    {
+      id: "design",
+      title:"Design",
+    },
+    {
+      id: "content",
+      title:"Content",
+    },
+  ];
+
+useEffect(()=>{
+
+  switch(selected){
+    case "featured":
+        setData(featuredPortfolio)
+        break;
+    case "web":
+        setData(webPortfolio)
+        break;
+    case "mobile":
+        setData(mobilePortfolio)
+        break;
+    case "design":
+        setData(designPortfolio)
+        break;
+    case "content":
+        setData(contentPortfolio)
+        break;
+    default:
+          setData(featuredPortfolio)
+  }
+}, [selected])
+
   return (
     <div className="portfolio" id="portfolio">
         <h1>Portfolio</h1>
         <ul>
-          <li className="active">Featured</li>
-          <li>Web Application</li>
-          <li>Mobile Application</li>
-          <li>Design</li>
-          <li>Branding</li>
+         {list.map((item) => (
+           <PortfolioList title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
+         ))}
         </ul>
         <div className="container">
+          {data.map((d)=> (
+
           <div className="item">
-            <img src="https://images.unsplash.com/photo-1565788061370-4771680a4df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=835&q=80"
+            <img src={d.img}
              alt="hey" 
              />
-            <h3>Banking App</h3>
+            <h3>{d.title}</h3>
           </div>
-          <div className="item">
-            <img src="https://images.unsplash.com/photo-1565788061370-4771680a4df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=835&q=80"
-             alt="hey" 
-             />
-            <h3>Banking App</h3>
-          </div>
-          <div className="item">
-            <img src="https://images.unsplash.com/photo-1565788061370-4771680a4df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=835&q=80"
-             alt="hey" 
-             />
-            <h3>Banking App</h3>
-          </div>
-          <div className="item">
-            <img src="https://images.unsplash.com/photo-1565788061370-4771680a4df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=835&q=80"
-             alt="hey" 
-             />
-            <h3>Banking App</h3>
-          </div>
-          <div className="item">
-            <img src="https://images.unsplash.com/photo-1565788061370-4771680a4df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=835&q=80"
-             alt="hey" 
-             />
-            <h3>Banking App</h3>
-          </div>
-          <div className="item">
-            <img src="https://images.unsplash.com/photo-1565788061370-4771680a4df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=835&q=80"
-             alt="hey" 
-             />
-            <h3>Banking App</h3>
-          </div>
+          
+          ))}
+          
         </div>
     </div>
   )
--- app/src/components/works/Works.jsx
+++ app/src/components/works/Works.jsx
@@ -3,7 +3,30 @@ import "./works.scss"
 export default function Works() {
   return (
     <div className="works" id="works">
-        
+        <div className="slider">
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                  <div className="leftContainer">
+                    <div className="imgContainer">
+                      <img src="assets/mobile.png" alt="" />
+                    </div>
+                  </div>
+                  <h2>Title</h2>
+                  <p>
+                    Random stuff here
+                  </p>
+                  <span>Projects</span>
+              </div>
+              <div className="right">
+
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
     </div>
   )
 }
